29fcba5dff172595bab17c371454f11bddbc6b66
Micro <Micro@DESKTOP-8VAHRK4>
2021-05-03 11:23:13 -0300

    Adicionando data de nascimento

--- primeiro-programa-java/src/curso/java/geovane/Pessoa.java
+++ primeiro-programa-java/src/curso/java/geovane/Pessoa.java
@@ -3,6 +3,13 @@ package curso.java.geovane;
 public class Pessoa {
 	private String nome;
 	private String sobrenome;
+	private int idade;
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getNome() {
 		return nome;
 	}
